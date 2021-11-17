e36357d5856e7962dfbad47a3a641095a15deba2
HEve <yh3413@columbia.edu>
2021-11-16 20:59:41 -0500

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -10,4 +10,13 @@ def test_buy_two_stock():
     p = Portfolio()
     p.buy("IBM", 100, 176.48)
     p.buy("HPQ", 100, 36.15)
-    assert p.cost() == 777
+    assert p.cost() == 21263.0
+
+
+
+def test_buy_three_stock():
+    p = Portfolio()
+    p.buy("IBM", 100, 0)
+    p.buy("HYW", 100, 0)
+    p.buy("GLZ", 100, 0)
+    assert p.cost() == 0
